5e93e22e75071bc9103d3cad968409740363ddd3
Azaria <rayandhika@students.amikom.ac.id>
2025-05-11 21:53:57 +0700

    persiapan project

---
+++ Project_coba.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string nama = "Asep";
+  int umur = 25;
+
+  std::cout << "Halo, nama saya " << nama << " dan umur saya " << umur << " tahun." << std::endl;
+
+  return 0;
+}
